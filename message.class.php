1d7bee29899e535bbfa9a8f9c948a4c18d7610b3
Gajus Kuizinas <g.kuizinas@anuary.com>
2013-07-27 19:39:20 +0000

    Implemented Message send method.

--- message.class.php
+++ message.class.php
@@ -2,35 +2,49 @@
 namespace ay\nexmo;
 
 class Message {
+	private
+		$url,
+		$key,
+		$secret;
 	
-}
-
-#$parameters = ['api_key' => '', 'api_secret' => '69c35f29', 'from' => 'MAMA', 'to' => '+447583246352', 'text' => 'System notification test.'];
+	public function __construct ($url, $key, $secret) {
+		$this->url = $url;
+		$this->key = $key;
+		$this->secret = $secret;
+	}
+	
+	public function send ($from, $to, $message) {
+		return $this->sendRequest(['from' => $from, 'to' => $to, 'text' => $message]);
+	}
+	
+	private function sendRequest (array $parameters) {
+		$ch = curl_init();
 		
-// @see https://mandrillapp.com/api/docs/messages.JSON.html
+		if (!isset($parameters['from'])) {
+			throw new \ErrorException('Originator name is required.');
+		}
+		
+		$parameters = ['api_key' => $this->key, 'api_secret' => $this->secret] + $parameters;
 
-
-$ch = curl_init();
-
-curl_setopt_array($ch, [
-	CURLOPT_URL => 'https://rest.nexmo.com/sms/json',
-	CURLOPT_TIMEOUT => 5,
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_SSL_VERIFYHOST => 2,
-	CURLOPT_SSL_VERIFYPEER => true,
-	CURLOPT_POST => true,
-	CURLOPT_HTTPHEADER => ['Content-Type: application/json'],
-	CURLOPT_POSTFIELDS => json_encode($parameters)
-]);
-
-$response = curl_exec($ch);
-
-ay($response);
-
-if (curl_errno($ch)) {			
-	throw new Error_Exception(error_get_last()['message']);
+		curl_setopt_array($ch, [
+			CURLOPT_URL => $this->url,
+			CURLOPT_TIMEOUT => 5,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYHOST => 2,
+			CURLOPT_SSL_VERIFYPEER => true,
+			CURLOPT_POST => true,
+			CURLOPT_HTTPHEADER => ['Content-Type: application/json'],
+			CURLOPT_POSTFIELDS => json_encode($parameters)
+		]);
+		
+		$response = curl_exec($ch);
+		
+		if (curl_errno($ch)) {
+			throw new \ErrorException(curl_error($ch));
+		}
+		
+		curl_close($ch);
+		
+		return json_decode($response, true);
+	}
 }
-
-curl_close($ch);
-
-return json_decode($response, true);
